240ed16eaf131ef776b13b18cfaca2d7ee4ac171
Tunc Kayikcioglu <19364610+tuncK@users.noreply.github.com>
2023-05-02 07:55:01 +0200

    add run script

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ for dim in 32 64 256 512; do
         python DM.py -d abundance_$dataset $group_opts ${common_opts}
         python DM.py -d marker_$dataset $group_opts ${common_opts}
         python DM.py -d combined_$dataset $group_opts ${common_opts}
-        mv results results_${dim}_$dataset}
+        mv results results_${dim}_$dataset
     done
 done
 
